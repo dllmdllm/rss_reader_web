0ffe86a70b7ff9e3d9fa1f260e50104d110e6e83
NM <nary@NMWindows365.localdomain>
2026-01-06 07:06:41 +0000

    Remove meta refresh 2026-01-05 12:20

--- generate_site.py
+++ generate_site.py
@@ -1238,7 +1238,6 @@ def build_html(
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="refresh" content="0">
   <title>即時焦點</title>
   <style>
     :root {{
